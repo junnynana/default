389dabc394e783c7f5186952782963e439d36c25
junnynana <yejunkim1102@gmail.com>
2025-09-02 02:19:50 +0900

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  <div className="app-wrap">
+    <h1 className="app">
+      <header className="app-header">
+        <logoArea/>
+        <profileArea/>
+        <menuButton/>
+        <menuButton/>
+        <menuButton/>
+        <menuButton/>
       </header>
-    </div>
+    </h1>
+    <body className="app-container">
+        <tasksArea/>
+    </body>
+  </div>
   );
 }
 
-export default App;
+function logoArea() {
+  return (
+    <div className="logo-wrap">
+      <div className="logo-area">
+        <div className="brand-name__area">
+          <div className="brand-name">
+            <div className="default-icon__area">
+              <i className="default-icon"/>
+            </div>
+            <div className="default-brand__name__area">
+              <div className="default-brand__name">
+                COMPANY
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function profileArea() {
+  return (
+  <div className="profileArea-wrap">
+    <div className="profileArea__area">
+      <div className="user-text__area">
+        <p className="user-text">
+          Hi, <span className="user-name__data">JJ.Lee!</span>
+        </p>
+      </div>
+      <div className="user-profile__area">
+        <div className="user-profile__area">
+          <i className="user-profile"/>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+function menuButton() {
+  return (
+    <div className="menuButton-wrap">
+      <button className="menuButton">
+        <a href="#" class="menuButton">.</a>
+      </button>
+    </div>
+  )
+}
+
+function menuButton1({ children, variant = "default", onClick }) {
+  return (
+    <button className={`btn btn-${variant}`} onClick={onClick}>
+      {children}
+    </button>
+  )
+}
+
+function tasksArea() {
+  return (
+    <div className="tasksArea-wrap">
+    </div>
+  )
+}
